09cd090d9976e4bae236e1e31ed9fb4f02d710af
thanhboo <thanhbtse02590@gmail.com>
2024-01-26 18:51:54 +0700

    Update modal

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 document.getElementById("modalBtn").addEventListener("click", function() {
     document.getElementById("myModal").style.display = "block";
+    document.body.classList.add('fixed');
   });
   
   document.getElementsByClassName("close")[0].addEventListener("click", function() {
     document.getElementById("myModal").style.display = "none";
+    document.body.classList.remove('fixed');
   });
+
+
   
