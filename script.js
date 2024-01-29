d2963277aa0675dc3a2b9636ebb90181f72c81f0
thanhboo <thanhbtse02590@gmail.com>
2024-01-29 09:56:44 +0700

    Fix body when open modal

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 document.getElementById("modalBtn").addEventListener("click", function() {
     document.getElementById("myModal").style.display = "block";
-    document.body.classList.add('fixed');
+    document.body.classList.add('modal-open');
+    // var scrollDistance = window.scrollT
   });
   
   document.getElementsByClassName("close")[0].addEventListener("click", function() {
     document.getElementById("myModal").style.display = "none";
-    document.body.classList.remove('fixed');
+    document.body.classList.remove('modal-open');
   });
 
 
