c01c64e99351323c0ce62a6a71e3425146dc39d1
thanhboo <thanhbtse02590@gmail.com>
2024-01-29 10:12:32 +0700

    Add window scrollTo
    Add window scrollTo prevent scrollTop after close modal

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-document.getElementById("modalBtn").addEventListener("click", function() {
-    document.getElementById("myModal").style.display = "block";
-    document.body.classList.add('modal-open');
-    // var scrollDistance = window.scrollT
-  });
-  
-  document.getElementsByClassName("close")[0].addEventListener("click", function() {
+var scrollDistance = window.scrollY;
+
+document.getElementById("modalBtn").addEventListener("click", function () {
+  document.getElementById("myModal").style.display = "block";
+  document.body.classList.add("modal-open");
+});
+
+document
+  .getElementsByClassName("close")[0]
+  .addEventListener("click", function () {
     document.getElementById("myModal").style.display = "none";
-    document.body.classList.remove('modal-open');
+    window.scrollTo(0, scrollDistance);
+    document.body.classList.remove("modal-open");
   });
-
-
-  
