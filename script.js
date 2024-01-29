64e3965f832d954be9f7b65e2cc5948e2f144758
thanhboo <thanhbtse02590@gmail.com>
2024-01-29 10:44:33 +0700

    Add files via upload
    asd

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ var scrollDistance = window.scrollY;
 
 document.getElementById("modalBtn").addEventListener("click", function () {
   document.getElementById("myModal").style.display = "block";
+  scrollDistance = window.scrollY;
+  console.log(scrollDistance * -1);
+  document.body.style.top = `${scrollDistance * -1}px`;
   document.body.classList.add("modal-open");
 });
 
 document
   .getElementsByClassName("close")[0]
   .addEventListener("click", function () {
-    document.getElementById("myModal").style.display = "none";
-    window.scrollTo(0, scrollDistance);
     document.body.classList.remove("modal-open");
+    window.scrollTo(0, scrollDistance);
+    document.getElementById("myModal").style.display = "none";
   });
